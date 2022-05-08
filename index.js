1e09762743b7601122dcbcb5b01765dc710b44af
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-08 18:22:33 +0600

    jwt

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyToken(req,res,next){
+  const authHeader=req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message:"unauthorized access"});
+  }
+  const token=authHeader.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(403).send({message:"Forbideen access"})
+    }
+    req.decoded=decoded;
+  })
+  next()
+
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awfqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -46,10 +62,16 @@ async function run() {
 
     })
     //post
-    app.post("/inventors", async (req, res) => {
-      const newInventor = req.body;
+    app.post("/inventors",verifyToken, async (req, res) => { 
+      const decodedEmail =req.decoded.email;
+      const email=req.body.email;
+      if (email===decodedEmail) {
+        const newInventor = req.body;
       const result = await ItemsCollection.insertOne(newInventor);
       res.send(result);
+      }else{
+        res.status(403).send({message:"Forbidden access"})
+      }
     });
     //delete
     app.delete("/inventors/:id", async (req, res) => {
@@ -59,7 +81,7 @@ async function run() {
       res.send(deleted);
     });
     //update
-    app.put("/inventors/:id", async (req, res) => {
+    app.put("/inventors/:id",verifyToken, async (req, res) => {
       const id = req.params.id;
       const updateInventor = req.body;
       const filter = { _id: ObjectId(id) };
