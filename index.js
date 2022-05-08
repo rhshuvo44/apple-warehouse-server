e4ff7eb184be16e2ed77b803e3e70093948a65a7
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-08 12:01:59 +0600

    root api

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ async function run() {
 run().catch(console.dur);
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Iphone-warehouse start");
 });
 app.listen(port, () => {
   console.log(`iphone-warehouse start ${port}`);
