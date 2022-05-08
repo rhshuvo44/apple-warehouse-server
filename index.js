e30389eb795c758efac575d7f0d3b30f90d57461
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-05-08 18:35:29 +0600

    jwt

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-function verifyToken(req,res,next){
-  const authHeader=req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message:"unauthorized access"});
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  const token=authHeader.split(' ')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
-    if(err){
-      return res.status(403).send({message:"Forbideen access"})
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbideen access" });
     }
-    req.decoded=decoded;
-  })
-  next()
-
+    req.decoded = decoded;
+  });
+  next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awfqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -44,34 +43,27 @@ async function run() {
       const items = await cursor.toArray();
       res.send(items);
     });
-    // single data load 
+    // single data load
     app.get("/inventors/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const item = await ItemsCollection.findOne(query);
       res.send(item);
     });
-    // auth 
-    app.post('/login',async(req,res)=>{
-      const user=req.body;
-      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:'1d'
+    // auth
+    app.post("/login", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
       });
-      
-      res.send({token})
 
-    })
+      res.send({ token });
+    });
     //post
-    app.post("/inventors",verifyToken, async (req, res) => { 
-      const decodedEmail =req.decoded.email;
-      const email=req.body.email;
-      if (email===decodedEmail) {
+    app.post("/inventors", async (req, res) => {
         const newInventor = req.body;
-      const result = await ItemsCollection.insertOne(newInventor);
-      res.send(result);
-      }else{
-        res.status(403).send({message:"Forbidden access"})
-      }
+        const result = await ItemsCollection.insertOne(newInventor);
+        res.send(result);
     });
     //delete
     app.delete("/inventors/:id", async (req, res) => {
@@ -81,7 +73,7 @@ async function run() {
       res.send(deleted);
     });
     //update
-    app.put("/inventors/:id",verifyToken, async (req, res) => {
+    app.put("/inventors/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const updateInventor = req.body;
       const filter = { _id: ObjectId(id) };
